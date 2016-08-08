86a2930bbc193e1b14e03a4f5587186df56f5312
rafidShahriar <oporajeyo21@gmail.com>
2016-08-08 22:10:44 +0600

    Dashboad modified

--- views/dashboard.php
+++ views/dashboard.php
@@ -19,55 +19,7 @@ if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
     </head>
     <body>
 
-    <div class="container">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Brand</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="dashboard.php">Dashboard <span class="sr-only">(current)</span></a></li>
-                        <li><a href="profile.php"> Profile </a></li>
-                        <li class="dropdown">
-
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Search</button>
-                    </form>
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-haspopup="true"
-                               aria-expanded="false">
-                                <?php
-                                echo ucfirst($_SESSION['user']['username']);
-                                ?>
-                                <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="logout.php">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-    </div>
+    <?php include_once 'includes/dashboard_menu.php';?>
     <div class="container">
         <h1>Welcome to My Application......This is admin area.</h1>
     </div>
